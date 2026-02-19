e3d90f17717efd118de480785ae7c21bff60aeef
Rijul Chaturvedi <rijchat@gmail.com>
2026-02-18 18:19:35 -0800

    Initial Commit

---
+++ ProteinLog/ProteinLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinLogApp.swift
+//  ProteinLog
+//
+//  Created by Rijul Chaturvedi on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProteinLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
